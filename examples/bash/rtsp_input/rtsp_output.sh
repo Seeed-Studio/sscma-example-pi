1093d044288efeec45d5cab6b00d47e658ac0e1e
qian <tangruiqian17@gmail.com>
2023-11-30 08:50:47 +0000

    example: update rtsp demo

--- examples/bash/rtsp_input/rtsp_output.sh
+++ examples/bash/rtsp_input/rtsp_output.sh
@@ -1,5 +1,6 @@
-./test-launch "( v4l2src name=cam_src ! videoconvert ! videoscale ! \
-  videoconvert ! x264enc ! video/x-h264,profile=baseline ! \
+./test-launch "( v4l2src name=cam_src ! v4l2convert ! videoscale ! \
+  v4l2convert ! queue ! v4l2h264enc ! 'video/x-h264,level=(string)4.2' ! \
+  v4l2convert ! video/x-h264,profile=baseline ! \
   h264parse ! rtph264pay name=pay0 pt=96 )"
 
 
--- examples/bash/rtsp_input/run.sh
+++ examples/bash/rtsp_input/run.sh
@@ -2,5 +2,4 @@
   rtspsrc location=rtsp://127.0.0.1:8554/test ! \
     rtph264depay ! h264parse config-interval=1 ! v4l2h264dec ! v4l2convert ! videoscale ! \
     video/x-raw,width=1280,height=720,format=RGB ! queue ! \
-    sscma_yolov5 model=../net/epoch_300_float.ncnn.bin,../net/epoch_300_float.ncnn.param labels=../net/coco.txt ! \
     v4l2convert ! ximagesink sync=false
